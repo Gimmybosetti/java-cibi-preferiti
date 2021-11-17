1e476009c1dcbe3ba7d810e7d6823d3edb455d93
GBosetti <gianmarco.bosetti@gmail.com>
2021-11-17 15:22:49 +0100

    Fatta parte bonus con casistica n° elem pari o dispari

--- src/org/generation/italy/array/CibiPreferiti.java
+++ src/org/generation/italy/array/CibiPreferiti.java
@@ -7,6 +7,7 @@ public class CibiPreferiti {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
+		int med;
 		String[] favFoods = new String[5];
 		
 		System.out.print("Type in your top 5 favourite foods: \n1)");
@@ -20,9 +21,21 @@ public class CibiPreferiti {
 		System.out.print("5)");
 		favFoods[4] = scanner.nextLine();
 		
-		System.out.println("\nThe length of the array is: " + favFoods.length + ".");
+		System.out.println("\nThe length of the scoreboard is: " + favFoods.length + ".");
 		System.out.println("The food in first position is: " + favFoods[0] + ".");
 		System.out.println("The food in last position is: " + favFoods[4] + ".");
+		
+		//nel caso di array con numero di elementi dispari la mediana è l'elemento centrale
+		//nel caso di array con numero di elementi pari invece bisogna prendere quelli in posizione n/2 e (n/2 + 1)
+		//nel mio caso sono 5 gli elementi ma faccio entrambe le casistiche
+		
+		med = favFoods.length / 2;
+		
+		if ((favFoods.length % 2) == 0) {
+			System.out.println("\nThe middle food are: " + favFoods[med - 1] + " and " + favFoods[med]);
+		}else {
+			System.out.println("\nThe middle food is: " + favFoods[med]);
+		}
 		
 		scanner.close();
 
